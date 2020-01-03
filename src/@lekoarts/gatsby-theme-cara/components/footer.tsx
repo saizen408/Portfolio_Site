265b14e124a28a86c7cb6ed52b9c144c70257f1a
Eric Owusu <llkoole7@gmail.com>
2020-01-03 00:04:51 -0800

    inital commit

---
+++ src/@lekoarts/gatsby-theme-cara/components/footer.tsx
@@ -0,0 +1,67 @@
+/** @jsx jsx */
+import {
+  Footer as ThemeFooter,
+  Styled,
+  Flex,
+  useColorMode,
+  jsx
+} from "theme-ui";
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode();
+  const isDark = colorMode === `dark`;
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`);
+  };
+
+  return (
+    <ThemeFooter>
+      <button
+        sx={{
+          variant: `buttons.toggle`,
+          fontWeight: `semibold`,
+          display: `block`,
+          mx: `auto`,
+          mb: 3
+        }}
+        onClick={toggleColorMode}
+        type="button"
+        aria-label="Toggle dark mode"
+      >
+        {isDark ? `Light` : `Dark`}
+      </button>
+      Copyright &copy; {new Date().getFullYear()}. All rights reserved.
+      <br />
+      <Flex
+        sx={{
+          justifyContent: `center`,
+          alignItems: `center`,
+          mt: 3,
+          color: `text`,
+          fontWeight: `semibold`,
+          a: { color: `text` }
+        }}
+      >
+        <img
+          width="45"
+          height="50"
+          src="https://i.imgur.com/BsZSmRN.png"
+          alt="SMB Logo"
+        />
+        {` `}
+        <Styled.a
+          aria-label="Link to the theme's GitHub repository"
+          sx={{ ml: 2 }}
+          href="https://github.com/saizen408"
+        ></Styled.a>
+        <div sx={{ mx: 1 }}>by</div>
+        {` `}
+        <Styled.a aria-label="Link to the theme author's website" href="#">
+          Synergized Mind and Body
+        </Styled.a>
+      </Flex>
+    </ThemeFooter>
+  );
+};
+
+export default Footer;
